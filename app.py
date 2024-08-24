3c281016a0a37b11cca191343176fc458814db67
ncam6020 <150554197+ncam6020@users.noreply.github.com>
2024-08-23 21:12:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import streamlit as st
+import time
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import CharacterTextSplitter
